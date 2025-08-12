8979b6fa84e1e97a90b0fafa8cbe68c10c1042e1
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-08-12 21:28:32 +0600

    Add Signup page component and integrate route in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/login'
+import Signup from './pages/signup'
 
 import Footer from './components/footer'
 import Navbar from './components/Navbar'
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         {/* Add more routes as needed */}
       </Routes>
       <Footer />
