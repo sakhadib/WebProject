d380a5bac945070dc14393ef4d260acfca92f1fa
sakhadib <adibsakhawat@iut-dhaka.edu>
2025-08-12 21:19:07 +0600

    Add Footer component and integrate it into App; remove Navbar from Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
+import Footer from './components/footer'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -5,7 +5,6 @@ import Article from "../components/article";
 export default function Home(){
     return (
         <div>
-            <Navbar />
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex gap-8">
                 <FeedAside />
 
